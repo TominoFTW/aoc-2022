4d78343676dc50ec74c3b95f7a82f1da8dcc03a1
TominoFTW <behal.t7@gmail.com>
2022-12-01 10:57:52 +0100

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,44 @@
+print("****************************************")
+print("AOC 2022 - Day 1")
+print("****************************************")
+
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+lines.append('')
+
+
+# Part 1
+elfSum = 0
+getMax = 0
+for line in lines:
+    if line == '':
+        getMax = max(getMax, elfSum)
+        elfSum = 0
+    else:
+        elfSum += int(line)
+
+print("Elf with the most calories")        
+print(getMax)
+
+
+# Part 2
+elfSum = 0
+getFirst = 0
+getSecond = 0
+getThird = 0
+
+for line in lines:
+    if line == '':
+        if elfSum > getFirst:
+            getThird, getSecond, getFirst = getSecond, getFirst, elfSum
+        elif elfSum > getSecond:
+            getThird, getSecond = getSecond, elfSum
+        elif elfSum > getThird:
+            getThird = elfSum
+        elfSum = 0
+    else:
+        elfSum += int(line)
+
+print("Sum of the first 3 elves")      
+print(getFirst + getSecond + getThird)
